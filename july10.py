9de2a5b8da0dd9343b6a971cf93d3b05a1e0521a
ContestCoder <139045837+ContestCoder@users.noreply.github.com>
2023-07-10 01:50:24 +0530

    Create july10.py

---
+++ july10.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+class Solution:
+    def transpose(self, matrix, n):
+        # Write Your code here
+        for i in range(n):
+            for j in range(i+1,n):
+                temp=matrix[i][j]
+                matrix[i][j]=matrix[j][i]
+                matrix[j][i]=temp
+            
